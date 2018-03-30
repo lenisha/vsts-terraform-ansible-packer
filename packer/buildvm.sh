99ab1099ec8fb63a4812437d720d82ff84dfc6c7
Elena Neroslavskaya <lenisha@gmail.com>
2018-03-30 18:03:20 -0400

    switch to shell

---
+++ packer/buildvm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "************* set environment vars"
+export ARM_CLIENT_ID=$1
+export ARM_CLIENT_SECRET=$2
+export ARM_RESOURCE_GROUP=$3
+export ARM_STORAGE_ACCOUNT=$4
+export ARM_SUBSCRIPTION_ID=$5
+export ARM_TENANT_ID=$6
+
+
+
+rm packer-build-output.log
+
+echo "************* execute packer build"
+## execute packer build and sendout to packer-build-output file
+packer build ./app.json -var playbook_drop_path=$7
+
+ cat azuredeploy.parameters.json
