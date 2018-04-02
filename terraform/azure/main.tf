de417e8d6ef055543b76bb7ce509ece5e5fd907f
Elena Neroslavskaya <lenisha@gmail.com>
2018-04-01 22:10:00 -0400

    disk

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -164,18 +164,20 @@ resource "azurerm_managed_disk" "test" {
 
 # Create virtual machine
 resource "azurerm_virtual_machine" "demo_vm" {
-  name                  = "packerVM"
-  location              = "${azurerm_resource_group.demo_resource_group.location}"
-  resource_group_name   = "${azurerm_resource_group.demo_resource_group.name}"
-  network_interface_ids = ["${azurerm_network_interface.demo_nic.id}"]
-  vm_size               = "Standard_DS1_v2"
+  name                             = "packerVM"
+  location                         = "${azurerm_resource_group.demo_resource_group.location}"
+  resource_group_name              = "${azurerm_resource_group.demo_resource_group.name}"
+  network_interface_ids            = ["${azurerm_network_interface.demo_nic.id}"]
+  vm_size                          = "Standard_DS1_v2"
+  delete_os_disk_on_termination    = true
+  delete_data_disks_on_termination = true
 
   storage_image_reference {
     id = "${data.azurerm_image.image.id}"
   }
 
   storage_os_disk {
-    name              = ""
+    name              = "mydisk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
