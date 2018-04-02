191600d1e9672d769d6be237c5387575c59b557c
Elena Neroslavskaya <lenisha@gmail.com>
2018-04-01 21:08:51 -0400

    manageddiskname

--- terraform/apply.sh
+++ terraform/apply.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ls -la
-echo "************* execute terraform apply  terraform apply -auto-approve -var baked_image_url=$6"
+echo "************* execute terraform apply  terraform apply -auto-approve -var manageddiskname=$6"
 
 ## execute terrafotm build and sendout to packer-build-output
 export ARM_CLIENT_ID=$1
@@ -9,7 +9,7 @@ export ARM_SUBSCRIPTION_ID=$3
 export ARM_TENANT_ID=$4
 export ARM_ACCESS_KEY=$5
 
-terraform apply -auto-approve -var "baked_image_url=$6"
+terraform apply -auto-approve -var "manageddiskname=$6"
 terraform output vm_ip > inventory
 
 ##echo " ansible_user=azureuser" >> inventory
