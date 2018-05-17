cecb287b19c1f942cecf5c94e872c5b0ced353c9
Elena Neroslavskaya <lenisha@gmail.com>
2018-05-16 23:49:37 -0400

    add init access

--- terraform/init.sh
+++ terraform/init.sh
@@ -5,5 +5,6 @@ export ARM_CLIENT_ID=$1
 export ARM_CLIENT_SECRET=$2
 export ARM_SUBSCRIPTION_ID=$3
 export ARM_TENANT_ID=$4
+export ARM_ACCESS_KEY=$5
 
 terraform init -backend-config=backend.tfvars
